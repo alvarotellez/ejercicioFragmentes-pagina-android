fcc8ab2300f3a45fb10939039ebdb68140c81f7f
alvarotellez <alvarotellezhidalgo@gmail.com>
2016-11-24 13:51:58 +0100

    Cuarto Commit

---
+++ app/src/main/java/es/iesnervion/atellez/ejerciciofragments/futbolista.java
@@ -0,0 +1,57 @@
+package es.iesnervion.atellez.ejerciciofragments;
+
+/**
+ * Created by atellez on 24/11/16.
+ */
+
+public class futbolista {
+    private int id;
+    private int fotoFutbolista;
+    private String nombre;
+    private String equipo;
+    private int nacionalidad;
+
+    public futbolista(){
+
+    }
+    public futbolista(int id, int fotoFutbolista, String nombre, String equipo,int nacionalidad){
+        this.id = id;
+        this.fotoFutbolista = fotoFutbolista;
+        this.nombre = nombre;
+        this.equipo = equipo;
+        this.nacionalidad = nacionalidad;
+    }
+
+
+    public int getFotoFutbolista() {
+        return fotoFutbolista;
+    }
+
+    public void setFotoFutbolista(int fotoFutbolista) {
+        this.fotoFutbolista = fotoFutbolista;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(String equipo) {
+        this.equipo = equipo;
+    }
+
+    public int getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(int nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+}
---
+++ app/src/main/java/es/iesnervion/atellez/ejerciciofragments/jugadores.java
@@ -0,0 +1,26 @@
+package es.iesnervion.atellez.ejerciciofragments;
+
+/**
+ * Created by atellez on 24/11/16.
+ */
+
+public class jugadores {
+
+    public static futbolista [] jugadores = {
+            new futbolista(1,R.drawable.anonimo,"Sergio Agüero","Manchester City", R.drawable.argentina),
+            new futbolista(2,R.drawable.anonimo,"Gareth Bale","Real Madrid",R.drawable.wales),
+            new futbolista(3,R.drawable.anonimo,"Gianluigi Buffon","Juventus",R.drawable.italy),
+            new futbolista(4,R.drawable.anonimo,"Kevin De Bruyne","Manchester City",R.drawable.belgium),
+            new futbolista(5,R.drawable.anonimo,"Paulo Dybala","Juventus",R.drawable.italy),
+            new futbolista(6,R.drawable.anonimo,"Diego Godin","Atletico de Madrid",R.drawable.uruguay),
+            new futbolista(7,R.drawable.anonimo,"Antoine Griezmann","Atletico de Madrid",R.drawable.france),
+            new futbolista(8,R.drawable.anonimo,"Gonzalo Higuain","Juventus/Napoles",R.drawable.argentina),
+            new futbolista(9,R.drawable.anonimo,"Andres Iniesta","FCB Barcelona",R.drawable.spain),
+            new futbolista(10,R.drawable.anonimo,"Koke Resurrecion","Atletico de Madrid",R.drawable.spain),
+            new futbolista(11,R.drawable.anonimo,"Cristiano Ronaldo","Real Madrid",R.drawable.portugal),
+            new futbolista(12,R.drawable.anonimo,"Leo Messi","FCB Barcelona",R.drawable.argentina),
+            new futbolista(13,R.drawable.anonimo,"Luis Suarez","FCB Barcelona",R.drawable.uruguay),
+            new futbolista(14,R.drawable.anonimo,"Neymar Jr","FCB Barcelona",R.drawable.brazil),
+            new futbolista(15,R.drawable.anonimo,"Sergio Ramos","Real Madrid",R.drawable.spain)
+    };
+}
